54a0d0648f8ecd1e8ef1b0a8d0f1eb3af4e6c557
caveman747 <kaladinstormblessed33@gmail.com>
2022-08-05 16:56:08 -0700

    Good for now see, add a section in CreateAccount for password change but still query the user for it of course. May want to add input validation for lower case if I'm feeling like it

--- main.py
+++ main.py
@@ -2,8 +2,16 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import tkinterclasses
 
 
+def main():
 
+    test = tkinterclasses.StartingBlock()
+
+    test.mainloop()
+
+if __name__ == "__main__":
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- tkinterclasses.py
+++ tkinterclasses.py
@@ -9,7 +9,7 @@ from tkinter.ttk import Progressbar
 #the skeleton of this is ripped directly from the answer to this stack overflow question
 #https://stackoverflow.com/questions/63017238/how-to-switch-between-different-tkinter-canvases-from-a-start-up-page-and-return
 
-class AdJoiner(tk.Tk):
+class StartingBlock(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self._mainCanvas = None
@@ -44,25 +44,51 @@ class AdJoiner(tk.Tk):
 
 class StartUpPage(tk.Canvas):
     def __init__(self, master, *args, **kwargs):
-        tk.Canvas.__init__(self, master, *args, **kwargs)
-        tk.Frame(self)  # Here the parent of the frame is the self instance of type tk.Canvas
-        tk.Label(self, text="Welcome to StartingBlock! \n This program was made to help setting up a Gitlab Runner that much easier \n Click Get Started to download dependencies"
-                 ).grid(column=0, row=0)
-        tk.Label(self, text="This program was made to help setting up a Gitlab Runner that much easier")
+        tk.Frame.__init__(self, master, *args, **kwargs)
+        self.canvas = tk.Canvas(self, height=200 ,width=430)
 
-        def DownloadDeps():
+        Explanation = tk.Label(self.canvas, text="Welcome to StartingBlock! \n This program was made to help setting up a Gitlab Runner that much easier \n Click Get Started to download dependencies")
+        Explanation.pack()
 
-            list = ["git", "curl"]
+        def installPackages():
+            list = ["curl", "git"]
             for i in list:
-                subprocess.run(["sudo", "apt", "install", i])
-                self.canvas.update_idletasks()
+                subprocess.run(["apt-get", "install", i])
                 pb["value"] += 50
+                self.canvas.update_idletasks()
                 txt["text"] = pb['value'], '%'
+                self.canvas.update_idletasks()
+
 
         pb = Progressbar(self.canvas, orient=tk.HORIZONTAL, length=100, mode="determinate")
         pb.pack()
 
+        txt = tk.Label(self.canvas, text="0%", bg="#345",fg="#fff")
+        txt.pack()
 
-        tk.Button(self, text="Download and install dependencies",
-                  command=lambda: master.switch_Canvas(DownloadDeps())).grid(column=0, row=1)
+        StartDownload = tk.Button(self.canvas, text="Install required packages and move on to the next step", command= lambda: [installPackages(), master.switch_Canvas(CreateUser)])
+        StartDownload.pack()
 
+
+        self.canvas.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+
+class CreateUser(tk.Canvas):
+    def __init__(self, master, *args, **kwargs):
+        tk.Frame.__init__(self, master, *args, **kwargs)
+        self.canvas = tk.Canvas(self, height=200 ,width=430)
+
+        Explanation = tk.Label(self.canvas, text="Next up, we need to create user account explicitly for the Gitlab Runner software")
+        Explanation.pack()
+
+        self.canvas.usernameEntry = tk.Entry(self.canvas)
+        self.canvas.usernameEntry.pack()
+        def CreateAccount():
+            username = self.canvas.usernameEntry.get()
+            subprocess.run(["sudo", "useradd", "--comment", username, "--create-home", username, "--shell", "/bin/bash"])
+
+        CreateAccountButton = tk.Button(self, text="Create Account", command= lambda: CreateAccount())
+        CreateAccountButton.pack()
+
+
+        self.canvas.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
